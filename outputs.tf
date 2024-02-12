6ed6258e62b3b260d934da413bca8072d3c030f6
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-02-12 22:05:30 +0200

    Update output

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "web_instance_ip" {
     description = "API Gateway URL to access the web page"
-    value = "http://${aws_route53_record.record.alias[0].name}"
+    value = aws_route53_record.record.alias[*].name
 }
