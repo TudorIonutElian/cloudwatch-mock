15f29da1030d89d5e1b263942a72d2084df8add1
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-02-12 14:27:59 +0200

    Add lambda to the party

---
+++ lambda.tf
@@ -0,0 +1,29 @@
+data "archive_file" "lambda_zip" {
+  type = "zip"
+  source_file = "index.js"
+  output_path = "index.zip"
+}
+
+resource "aws_lambda_function" "cloudwatch_lambda" {
+  filename = "index.zip"
+  function_name = "cloudWatchLambda"
+  role = aws_iam_role.cloudwatch_lambda_role.arn
+  handler = "index.handler"
+  runtime = "nodejs18.x"
+  source_code_hash = data.archive_file.lambda_package.output_base64sha256
+}
+
+resource "aws_iam_role" "cloudwatch_lambda_role" {
+  name = "lambda-role"
+
+  assume_role_policy = jsonencode({
+        Version = "2012-10-17",
+        Statement = [{
+            Action = "sts:AssumeRole",
+            Effect = "Allow",
+            Principal = {
+                Service = "lambda.amazonaws.com"
+            }
+        }]
+    })
+}
