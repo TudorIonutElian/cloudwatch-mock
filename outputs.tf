7598c5e4d678dad36bd85866f2b8430c36d3d7af
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-02-14 23:05:54 +0200

    New output update

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "api_gateway_url" {
     description = "API Gateway URL to access the web page"
-    value = "${aws_api_gateway_rest_api.cloudwatch_mock_api.execution_arn}/*/*"
+    value = "https://${aws_api_gateway_rest_api.cloudwatch_mock_api.id}.execute-api.eu-central-1.amazonaws.com/${aws_api_gateway_deployment.deployment.stage_name}"
 }
