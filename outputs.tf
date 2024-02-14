d09dbe4c7e3f90446a35f13bc680a0ccd06f303e
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-02-14 23:08:31 +0200

    Update to print path

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
-output "api_gateway_url" {
+output "API_DEMO_PATH_METHOD" {
     description = "API Gateway URL to access the web page"
-    value = "https://${aws_api_gateway_rest_api.cloudwatch_mock_api.id}.execute-api.eu-central-1.amazonaws.com/${aws_api_gateway_deployment.deployment.stage_name}"
+    value = "https://${aws_api_gateway_rest_api.cloudwatch_mock_api.id}.execute-api.eu-central-1.amazonaws.com/${aws_api_gateway_deployment.deployment.stage_name}/${aws_api_gateway_resource.root.path_part}"
 }
