23a94dc1ecec1846e4f57206129f6423b49bfc98
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-02-14 22:40:05 +0200

    Update lambda getter

--- setup-project.sh
+++ setup-project.sh
@@ -4,3 +4,6 @@ sudo apt-get install curl
 curl -L -o example-lambda.zip https://github.com/TudorIonutElian/example-lambda/archive/refs/heads/main.zip
 
 unzip example-lambda.zip -d example-lambda
+
+mv example-lambda/example-lambda-main/* example-lambda/
+rm -r example-lambda/example-lambda-main
