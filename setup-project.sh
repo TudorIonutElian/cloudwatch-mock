47587b8aa2dec206cd9cca65c859b959fb0f5b95
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-02-15 20:06:25 +0200

    install nodejs

--- setup-project.sh
+++ setup-project.sh
@@ -1,5 +1,6 @@
 sudo apt-get install unzip
 sudo apt-get install curl
+sudo apt-get install nodejs
 
 curl -L -o example-lambda.zip https://github.com/TudorIonutElian/example-lambda/archive/refs/heads/main.zip
 
@@ -8,6 +9,9 @@ unzip example-lambda.zip -d example-lambda
 mv example-lambda/example-lambda-main/* example-lambda/
 rm -r example-lambda/example-lambda-main
 rm example-lambda.zip
+
+cd example-lambda
+
 
 terraform init
 terraform plan
