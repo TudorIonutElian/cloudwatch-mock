78f025ca61b69bde3082958ddeba3403feb5b3ec
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-02-17 00:29:21 +0200

    Separate roles

--- variables.tf
+++ variables.tf
@@ -1,3 +1,6 @@
+/*
+ * This file contains the variables for the S3 bucket
+ */
 variable cloudwatch_mock_lambda_bucket_name {
     description = "Value for the bucket name"
     default = "cloudwatch-mock-lambda-bucket"
