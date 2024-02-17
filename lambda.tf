449c8583ad746631a47d466ec2faee6cd9dd2a18
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-02-17 22:45:14 +0200

    Add write-logs-func

--- lambda.tf
+++ lambda.tf
@@ -4,14 +4,21 @@
  * The role is created and attached to the lambda function.
  * The role has a policy attached to it which allows the lambda function to access the S3 bucket.
  */
-data "archive_file" "lambda_function_zip" {
+data "archive_file" "write_payload_func_zip" {
   type        = "zip"
-  source_dir  = "example-lambda"
-  output_path = "index.zip"
+  source_dir  = "write-payload-func"
+  output_path = "write_payload_func_zip.zip"
+}
+
+data "archive_file" "write_logs_func_zip" {
+  type        = "zip"
+  source_dir  = "write-logs-func"
+  output_path = "write_logs_func_zip.zip"
 }
 
 /*******************************************************
  * This resource is used to create the lambda function.
+        for writing the payload to the S3 bucket.
 *******************************************************/
 
 resource "aws_lambda_function" "write_payload_func" {
@@ -20,5 +27,19 @@ resource "aws_lambda_function" "write_payload_func" {
   role             = aws_iam_role.write_payload_func_role.arn
   handler          = "index.handler"
   runtime          = "nodejs18.x"
-  source_code_hash = data.archive_file.lambda_function_zip.output_base64sha256
+  source_code_hash = data.archive_file.write_payload_func_zip.output_base64sha256
+}
+
+
+/*******************************************************
+ * This resource is used to create the lambda function.
+*******************************************************/
+
+resource "aws_lambda_function" "write_logs_func" {
+  filename         = "write_logs_func.zip"
+  function_name    = "write-logs-func"
+  role             = aws_iam_role.write_logs_func_role.arn
+  handler          = "index.handler"
+  runtime          = "nodejs18.x"
+  source_code_hash = data.archive_file.write_logs_func_zip.output_base64sha256
 }
