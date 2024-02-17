4da2aa3dfa90b3659448349d96ee185a998d74ff
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-02-17 23:03:47 +0200

    Update runner

--- runner.sh
+++ runner.sh
@@ -9,8 +9,12 @@ initial_setup
 # Call the prepare_lambda function
 getWritePayloadLambda
 
+cd ../
+
 # Call the prepare_lambda_write_logs_function
 getWriteLogsLambda
+
+cd ../
 
 # Call the terraform_init function
 terraform_init
