161cbe2d676139cdc7f51d6e5bf73ffd259d0574
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-02-17 21:54:52 +0200

    Add s3 notification

--- s3_bucket.tf
+++ s3_bucket.tf
@@ -4,3 +4,16 @@
 resource "aws_s3_bucket" "cloudwatch_mock_lambda_bucket" {
   bucket = var.cloudwatch_mock_lambda_bucket_name
 }
+
+
+resource "aws_s3_bucket_notification" "notif" {
+  bucket = aws_s3_bucket.bucket.id
+
+  topic {
+    topic_arn = aws_sns_topic.custom_cloudwatch_sns_topic.arn
+
+    events = [
+      "s3:ObjectCreated:*",
+    ]
+  }
+}
