061024cd5a580c07ce255926d18dfea512597fe0
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-02-17 22:48:46 +0200

    Add new lambda to file solver

--- runner.sh
+++ runner.sh
@@ -3,8 +3,14 @@
 # Import functions.sh from the ./scripts directory
 source ./scripts/functions.sh
 
+# Call the initial_setup function
+initial_setup
+
 # Call the prepare_lambda function
-prepare_lambda
+prepare_lambda_write_payload_function
+
+# Call the prepare_lambda_write_logs_function
+prepare_lambda_write_logs_function
 
 # Call the terraform_init function
 terraform_init
--- scripts/functions.sh
+++ scripts/functions.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-function prepare_lambda {
+function initial_setup {
     sudo apt-get install unzip
     sudo apt-get install curl
     sudo apt-get -y install nodejs npm
+}
+
+function prepare_lambda_write_payload_function {
 
     if [ -d "write-payload-func" ]; then rm -Rf write-payload-func; fi
 
@@ -16,6 +19,21 @@ function prepare_lambda {
     rm write-payload-func.zip
 
     cd write-payload-func
+    npm install
+}
+
+function prepare_lambda_write_logs_function {
+    if [ -d "write-logs-func" ]; then rm -Rf write-logs-func; fi
+
+    curl -L -o write-logs-func.zip https://github.com/TudorIonutElian/write-logs-func/archive/refs/heads/main.zip
+
+    unzip write-logs-func.zip -d write-logs-func
+
+    mv write-logs-func/write-logs-func-main/* write-logs-func/
+    rm -r write-logs-func/write-logs-func-main
+    rm write-logs-func.zip
+
+    cd write-logs-func
     npm install
 }
 
