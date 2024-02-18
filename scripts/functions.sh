214255870bcd53ad9c57936c976557e0dc8bfaa0
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-02-18 13:18:53 +0200

    Add logic to download getlogsfunc

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -6,7 +6,7 @@ function initial_setup {
     sudo apt-get -y install nodejs npm
 }
 
-function getWritePayloadLambda {
+function loadWritePayloadLambda {
 
     if [ -d "write-payload-func" ]; then rm -Rf write-payload-func; fi
 
@@ -23,7 +23,7 @@ function getWritePayloadLambda {
     cd ../
 }
 
-function getWriteLogsLambda {
+function loadWriteLogsLambda {
     if [ -d "write-logs-func" ]; then rm -Rf write-logs-func; fi
 
     curl -L -o write-logs-func.zip https://github.com/TudorIonutElian/write-logs-func/archive/refs/heads/main.zip
@@ -39,7 +39,7 @@ function getWriteLogsLambda {
     cd ../
 }
 
-function getGegLogsLambda {
+function loadGegLogsLambda {
     if [ -d "get-logs-func" ]; then rm -Rf get-logs-func; fi
 
     curl -L -o get-logs-func.zip https://github.com/TudorIonutElian/get-logs-func/archive/refs/heads/main.zip
