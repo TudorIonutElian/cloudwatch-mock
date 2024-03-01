98a3f760133e825af444e7528fea7c564cb96848
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-03-01 16:58:27 +0200

    Fix outputs

--- outputs.tf
+++ outputs.tf
@@ -3,7 +3,15 @@
  ****************************************************/
 output "API_DEMO_PATH_METHOD" {
   description = "API Gateway URL to access the web page"
-  value       = "https://${aws_api_gateway_rest_api.cloudwatch_mock_api.id}.execute-api.eu-central-1.amazonaws.com/${aws_api_gateway_deployment.deployment.stage_name}/${aws_api_gateway_resource.root.path_part}"
+  value       = "https://${aws_api_gateway_rest_api.cloudwatch_mock_api.id}.execute-api.eu-central-1.amazonaws.com/${aws_api_gateway_deployment.deployment.stage_name}/${aws_api_gateway_resource.demo.path_part}"
+}
+
+/****************************************************
+ * Output the API Gateway URL to access the API logs
+ ****************************************************/
+output "API_LOGS_PATH_METHOD" {
+  description = "API Gateway URL to access the logs"
+  value       = "https://${aws_api_gateway_rest_api.cloudwatch_mock_api.id}.execute-api.eu-central-1.amazonaws.com/${aws_api_gateway_deployment.deployment.stage_name}/${aws_api_gateway_resource.logs.path_part}"
 }
 
 /****************************************************
