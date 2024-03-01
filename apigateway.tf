31ba8dcef442658f919a818fe2b674c034414aac
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-03-01 16:55:20 +0200

    Fix namings

--- apigateway.tf
+++ apigateway.tf
@@ -86,7 +86,7 @@ resource "aws_api_gateway_resource" "logs" {
 /**********************************************************
 *** # Add first gateway METHOD
 **********************************************************/
-resource "aws_api_gateway_method" "proxy" {
+resource "aws_api_gateway_method" "proxy_logs" {
   rest_api_id   = aws_api_gateway_rest_api.cloudwatch_mock_api.id
   resource_id   = aws_api_gateway_resource.logs.id
   http_method   = "GET"
@@ -99,16 +99,16 @@ resource "aws_api_gateway_method" "proxy" {
 resource "aws_api_gateway_integration" "lambda_integration" {
   rest_api_id             = aws_api_gateway_rest_api.cloudwatch_mock_api.id
   resource_id             = aws_api_gateway_resource.logs.id
-  http_method             = aws_api_gateway_method.proxy.http_method
+  http_method             = aws_api_gateway_method.proxy_logs.http_method
   integration_http_method = "GET"
   type                    = "AWS"
   uri                     = aws_lambda_function.get_logs_func.invoke_arn
 }
 
-resource "aws_api_gateway_method_response" "proxy" {
+resource "aws_api_gateway_method_response" "proxy_logs" {
   rest_api_id = aws_api_gateway_rest_api.cloudwatch_mock_api.id
   resource_id = aws_api_gateway_resource.logs.id
-  http_method = aws_api_gateway_method.proxy.http_method
+  http_method = aws_api_gateway_method.proxy_logs.http_method
   status_code = "200"
 
   response_parameters = {
@@ -118,11 +118,11 @@ resource "aws_api_gateway_method_response" "proxy" {
   }
 }
 
-resource "aws_api_gateway_integration_response" "proxy" {
+resource "aws_api_gateway_integration_response" "proxy_logs" {
   rest_api_id = aws_api_gateway_rest_api.cloudwatch_mock_api.id
   resource_id = aws_api_gateway_resource.logs.id
-  http_method = aws_api_gateway_method.proxy.http_method
-  status_code = aws_api_gateway_method_response.proxy.status_code
+  http_method = aws_api_gateway_method.proxy_logs.http_method
+  status_code = aws_api_gateway_method_response.proxy_logs.status_code
 
   response_parameters = {
     "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
@@ -131,7 +131,7 @@ resource "aws_api_gateway_integration_response" "proxy" {
   }
 
   depends_on = [
-    aws_api_gateway_method.proxy,
+    aws_api_gateway_method.proxy_logs,
     aws_api_gateway_integration.lambda_integration
   ]
 }
