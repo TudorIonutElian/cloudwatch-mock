49503778adfc9478bca6bf2b8cbc7885095b06f3
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-03-01 17:03:04 +0200

    Add integration

--- apigateway.tf
+++ apigateway.tf
@@ -141,7 +141,8 @@ resource "aws_api_gateway_integration_response" "proxy_logs" {
 **********************************************************/
 resource "aws_api_gateway_deployment" "deployment" {
   depends_on = [
-    aws_api_gateway_integration.lambda_integration
+    aws_api_gateway_integration.lambda_integration,
+    aws_api_gateway_integration.lambda_integration_logs
   ]
 
   rest_api_id = aws_api_gateway_rest_api.cloudwatch_mock_api.id
