5f91b383eabe182963dd97cec60567fb413bf03c
Ionut.Tudor <ionut.tudor@iris.co.uk>
2024-03-01 19:55:19 +0200

    Big changes

--- apigateway-getlogs.tf
+++ apigateway-getlogs.tf
@@ -1,48 +1,87 @@
-resource "aws_api_gateway_rest_api" "get_logs_api" {
-  name = "get-logs-api"
-  description = "get-logs-api"
+/**********************************************************
+  # Add the API Gatewy
+**********************************************************/
+
+resource "aws_api_gateway_rest_api" "cloudwatch_mock_api_2" {
+  name        = "cloudwatch-mock-api"
+  description = "CloudWatch Demo Mock"
 
   endpoint_configuration {
     types = ["REGIONAL"]
   }
 }
 
-resource "aws_api_gateway_resource" "root" {
-  rest_api_id = aws_api_gateway_rest_api.get_logs_api.id
-  parent_id = aws_api_gateway_rest_api.get_logs_api.root_resource_id
-  path_part = "logs"
+/**********************************************************
+*** # Add first resource to the cloudwatch_mock_api
+**********************************************************/
+resource "aws_api_gateway_resource" "logs" {
+  rest_api_id = aws_api_gateway_rest_api.cloudwatch_mock_api_2.id
+  parent_id   = aws_api_gateway_rest_api.cloudwatch_mock_api_2.root_resource_id
+  path_part   = "logs"
 }
 
-resource "aws_api_gateway_method" "proxy" {
-  rest_api_id = aws_api_gateway_rest_api.get_logs_api.id
-  resource_id = aws_api_gateway_resource.root.id
-  http_method = "GET"
+/**********************************************************
+*** # Add first gateway METHOD
+**********************************************************/
+resource "aws_api_gateway_method" "proxy_2" {
+  rest_api_id   = aws_api_gateway_rest_api.cloudwatch_mock_api_2.id
+  resource_id   = aws_api_gateway_resource.logs.id
+  http_method   = "GET"
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_integration" "lambda_integration" {
-  rest_api_id = aws_api_gateway_rest_api.get_logs_api.id
-  resource_id = aws_api_gateway_resource.root.id
-  http_method = aws_api_gateway_method.proxy.http_method
+/**********************************************************
+*** # Add lambda integration
+**********************************************************/
+resource "aws_api_gateway_integration" "lambda_integration_2" {
+  rest_api_id             = aws_api_gateway_rest_api.cloudwatch_mock_api_2.id
+  resource_id             = aws_api_gateway_resource.logs.id
+  http_method             = aws_api_gateway_method.proxy_2.http_method
   integration_http_method = "GET"
-  type = "MOCK"
+  type                    = "AWS"
+  uri                     = aws_lambda_function.get_logs_func.invoke_arn
 }
 
-resource "aws_api_gateway_method_response" "proxy" {
-  rest_api_id = aws_api_gateway_rest_api.get_logs_api.id
-  resource_id = aws_api_gateway_resource.root.id
-  http_method = aws_api_gateway_method.proxy.http_method
+resource "aws_api_gateway_method_response" "proxy_3" {
+  rest_api_id = aws_api_gateway_rest_api.cloudwatch_mock_api_2.id
+  resource_id = aws_api_gateway_resource.logs.id
+  http_method = aws_api_gateway_method.proxy_2.http_method
   status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true,
+    "method.response.header.Access-Control-Allow-Methods" = true,
+    "method.response.header.Access-Control-Allow-Origin"  = true
+  }
 }
 
-resource "aws_api_gateway_integration_response" "proxy" {
-  rest_api_id = aws_api_gateway_rest_api.get_logs_api.id
-  resource_id = aws_api_gateway_resource.root.id
-  http_method = aws_api_gateway_method.proxy.http_method
-  status_code = aws_api_gateway_method_response.proxy.status_code
+resource "aws_api_gateway_integration_response" "proxy_4" {
+  rest_api_id = aws_api_gateway_rest_api.cloudwatch_mock_api_2.id
+  resource_id = aws_api_gateway_resource.logs.id
+  http_method = aws_api_gateway_method.proxy_2.http_method
+  status_code = aws_api_gateway_method_response.proxy_3.status_code
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT'",
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+  }
 
   depends_on = [
-    aws_api_gateway_method.proxy,
-    aws_api_gateway_integration.lambda_integration
+    aws_api_gateway_method.proxy_2,
+    aws_api_gateway_integration.lambda_integration_2
   ]
+}
+
+
+/**********************************************************
+*** # Add lambda integration
+**********************************************************/
+resource "aws_api_gateway_deployment" "deployment_2" {
+  depends_on = [
+    aws_api_gateway_integration.lambda_integration_2,
+  ]
+
+  rest_api_id = aws_api_gateway_rest_api.cloudwatch_mock_api_2.id
+  stage_name  = "development"
 }
