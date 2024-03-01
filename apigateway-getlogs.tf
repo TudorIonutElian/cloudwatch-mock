4cf597d4e8c4d6c0ed788516bb0cead94a635f79
Ionut.Tudor <ionut.tudor@iris.co.uk>
2024-03-01 19:50:23 +0200

    Other changes

--- apigateway-getlogs.tf
+++ apigateway-getlogs.tf
@@ -1,48 +1,48 @@
-resource "aws_api_gateway_rest_api" "api_get_logs" {
+resource "aws_api_gateway_rest_api" "get_logs_api" {
   name = "get-logs-api"
-  description = "API for getting logs"
+  description = "get-logs-api"
 
   endpoint_configuration {
     types = ["REGIONAL"]
   }
 }
 
-resource "aws_api_gateway_resource" "logs" {
-  rest_api_id = aws_api_gateway_rest_api.api_get_logs.id
-  parent_id = aws_api_gateway_rest_api.api_get_logs.root_resource_id
+resource "aws_api_gateway_resource" "root" {
+  rest_api_id = aws_api_gateway_rest_api.get_logs_api.id
+  parent_id = aws_api_gateway_rest_api.get_logs_api.root_resource_id
   path_part = "logs"
 }
 
-resource "aws_api_gateway_method" "proxy_method_get_logs" {
-  rest_api_id = aws_api_gateway_rest_api.api_get_logs.id
-  resource_id = aws_api_gateway_resource.logs.id
+resource "aws_api_gateway_method" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.get_logs_api.id
+  resource_id = aws_api_gateway_resource.root.id
   http_method = "GET"
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_integration" "lambda_integration_logs" {
-  rest_api_id = aws_api_gateway_rest_api.api_get_logs.id
-  resource_id = aws_api_gateway_resource.logs.id
-  http_method = aws_api_gateway_method.proxy_method_get_logs.http_method
-  integration_http_method = "POST"
+resource "aws_api_gateway_integration" "lambda_integration" {
+  rest_api_id = aws_api_gateway_rest_api.get_logs_api.id
+  resource_id = aws_api_gateway_resource.root.id
+  http_method = aws_api_gateway_method.proxy.http_method
+  integration_http_method = "GET"
   type = "MOCK"
 }
 
-resource "aws_api_gateway_method_response" "proxy_method_response_logs" {
-  rest_api_id = aws_api_gateway_rest_api.api_get_logs.id
-  resource_id = aws_api_gateway_resource.logs.id
-  http_method = aws_api_gateway_method.proxy_method_get_logs.http_method
+resource "aws_api_gateway_method_response" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.get_logs_api.id
+  resource_id = aws_api_gateway_resource.root.id
+  http_method = aws_api_gateway_method.proxy.http_method
   status_code = "200"
 }
 
-resource "aws_api_gateway_integration_response" "proxy_integration_response_logs" {
-  rest_api_id = aws_api_gateway_rest_api.api_get_logs.id
-  resource_id = aws_api_gateway_resource.logs.id
-  http_method = aws_api_gateway_method.proxy_method_response_logs.http_method
-  status_code = aws_api_gateway_method_response.proxy_method_response_logs.status_code
+resource "aws_api_gateway_integration_response" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.get_logs_api.id
+  resource_id = aws_api_gateway_resource.root.id
+  http_method = aws_api_gateway_method.proxy.http_method
+  status_code = aws_api_gateway_method_response.proxy.status_code
 
   depends_on = [
-    aws_api_gateway_method.proxy_method_get_logs,
+    aws_api_gateway_method.proxy,
     aws_api_gateway_integration.lambda_integration
   ]
 }
