c2571c11a6abda222a657baae7b327f7fbe44f6a
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-03-01 22:15:56 +0200

    Add the proxy back

--- apigateway-demo.tf
+++ apigateway-demo.tf
@@ -101,7 +101,7 @@ resource "aws_api_gateway_integration" "lambda_integration_get_logs_func" {
   resource_id             = aws_api_gateway_resource.logs_resource.id
   http_method             = aws_api_gateway_method.proxy_aws_api_gateway_method_logs.http_method
   integration_http_method = "POST"
-  type                    = "AWS"
+  type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.get_logs_func.invoke_arn
 }
 
