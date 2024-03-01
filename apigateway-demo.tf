58046152b0c82dc7f3bedea19e39bec8b123703c
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-03-01 21:29:33 +0200

    aaaaaaaaa

--- apigateway-demo.tf
+++ apigateway-demo.tf
@@ -102,7 +102,7 @@ resource "aws_api_gateway_resource" "logs_resource" {
 resource "aws_api_gateway_method" "proxy_aws_api_gateway_method_logs" {
   rest_api_id   = aws_api_gateway_rest_api.cloudwatch_mock_api.id
   resource_id   = aws_api_gateway_resource.logs_resource.id
-  http_method   = "POST"
+  http_method   = "GET"
   authorization = "NONE"
 }
 
