539fd2e78bc4c93f15e8706165fd1bd1b0fb484f
Ionut.Tudor <ionut.tudor@iris.co.uk>
2024-03-01 20:30:12 +0200

    Remove second method

--- apigateway.tf
+++ apigateway.tf
@@ -21,13 +21,6 @@ resource "aws_api_gateway_resource" "demo" {
 }
 
 
-resource "aws_api_gateway_resource" "logs" {
-  rest_api_id = aws_api_gateway_rest_api.cloudwatch_mock_api.id
-  parent_id   = aws_api_gateway_rest_api.cloudwatch_mock_api.root_resource_id
-  path_part   = "logs"
-}
-
-
 /**********************************************************
 *** # Add first gateway METHOD
 **********************************************************/
@@ -37,14 +30,6 @@ resource "aws_api_gateway_method" "proxy" {
   http_method   = "POST"
   authorization = "NONE"
 }
-
-resource "aws_api_gateway_method" "proxy_logs" {
-  rest_api_id   = aws_api_gateway_rest_api.cloudwatch_mock_api.id
-  resource_id   = aws_api_gateway_resource.logs.id
-  http_method   = "GET"
-  authorization = "NONE"
-}
-
 
 /**********************************************************
 *** # Add lambda integration
@@ -58,32 +43,10 @@ resource "aws_api_gateway_integration" "lambda_integration" {
   uri                     = aws_lambda_function.write_payload_func.invoke_arn
 }
 
-resource "aws_api_gateway_integration" "lambda_integration_logs" {
-  rest_api_id             = aws_api_gateway_rest_api.cloudwatch_mock_api.id
-  resource_id             = aws_api_gateway_resource.logs.id
-  http_method             = aws_api_gateway_method.proxy_logs.http_method
-  integration_http_method = "GET"
-  type                    = "AWS"
-  uri                     = aws_lambda_function.get_logs_func.invoke_arn
-}
-
 resource "aws_api_gateway_method_response" "proxy" {
   rest_api_id = aws_api_gateway_rest_api.cloudwatch_mock_api.id
   resource_id = aws_api_gateway_resource.demo.id
   http_method = aws_api_gateway_method.proxy.http_method
-  status_code = "200"
-
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = true,
-    "method.response.header.Access-Control-Allow-Methods" = true,
-    "method.response.header.Access-Control-Allow-Origin"  = true
-  }
-}
-
-resource "aws_api_gateway_method_response" "proxy_logs_response" {
-  rest_api_id = aws_api_gateway_rest_api.cloudwatch_mock_api.id
-  resource_id = aws_api_gateway_resource.logs.id
-  http_method = aws_api_gateway_method.proxy_logs.http_method
   status_code = "200"
 
   response_parameters = {
@@ -110,26 +73,6 @@ resource "aws_api_gateway_integration_response" "proxy" {
     aws_api_gateway_integration.lambda_integration
   ]
 }
-
-resource "aws_api_gateway_integration_response" "proxy_integration_response" {
-  rest_api_id = aws_api_gateway_rest_api.cloudwatch_mock_api.id
-  resource_id = aws_api_gateway_resource.logs.id
-  http_method = aws_api_gateway_method.proxy_logs.http_method
-  status_code = aws_api_gateway_method_response.proxy_logs_response.status_code
-
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
-    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT'",
-    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
-  }
-
-  depends_on = [
-    aws_api_gateway_method.proxy_logs,
-    aws_api_gateway_integration.lambda_integration_logs
-  ]
-}
-
-
 
 
 /**********************************************************
