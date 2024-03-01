2058a1a459bb769bca2e7e9620adad0f7cab3d0b
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-03-01 21:52:18 +0200

    Comment something

--- apigateway-demo.tf
+++ apigateway-demo.tf
@@ -129,8 +129,7 @@ resource "aws_api_gateway_method_response" "proxy_aws_api_gateway_method_respons
     "method.response.header.Access-Control-Allow-Methods" = true,
     "method.response.header.Access-Control-Allow-Origin"  = true
   }
-}
-
+}/*
 resource "aws_api_gateway_integration_response" "proxy_2" {
   rest_api_id = aws_api_gateway_rest_api.cloudwatch_mock_api.id
   resource_id = aws_api_gateway_resource.logs_resource.id
@@ -148,7 +147,7 @@ resource "aws_api_gateway_integration_response" "proxy_2" {
     aws_api_gateway_integration.lambda_integration_get_logs_func
   ]
 }
-
+*/
 
 /**********************************************************
 *** # Add lambda integration
