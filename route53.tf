6cdf5946e280d3463fde20c0b1c739a8c4094608
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-03-10 00:34:45 +0200

    Connect app to cloudwatch

--- route53.tf
+++ route53.tf
@@ -22,10 +22,10 @@ resource "aws_route53_record" "api_domain_record" {
  * Create a record for the API Gateway
  ****************************************************/
 resource "aws_route53_record" "cloudwatch_domain_record" {
-  name = "cloud-watch" # The subdomain (api.sumeet.life)
-  type = "CNAME"
-  ttl  = "300" # TTL in seconds
-
-  records = ["cloudwatch.learndevtech.com"]
   zone_id = data.aws_route53_zone.learndevtech.zone_id
+  name    = "cloud-watch" // replace with your domain name
+  type    = "A"
+  ttl     = "300"
+  records = [aws_instance.iris_tf_demo_ec2_instance.public_ip]
 }
+
