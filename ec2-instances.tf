d80e7812bbc874af8be3bea0b24acc43dd6263f4
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-03-10 00:19:20 +0200

    Set user data in  strings

--- ec2-instances.tf
+++ ec2-instances.tf
@@ -24,7 +24,25 @@ resource "aws_instance" "iris_tf_demo_ec2_instance" {
   ami = data.aws_ami.iris_tf_demo_ec2_ami_filter.id
   instance_type   = "t2.micro"
   key_name = aws_key_pair.kp.key_name
-  user_data = file("scripts/cloudwatch_entry_script.sh")
+  user_data = <<-EOF
+                yum update -y
+                yum install -y httpd
+                yum install -y git
+                yum install -y nodejs 
+                yum install -y npm
+                systemctl start httpd
+                systemctl enable httpd
+
+                echo "export VARNAME=${output.API_LOGS_PATH_METHOD}" >> /etc/environment
+
+                cd /var/www/
+                git clone https://github.com/TudorIonutElian/cloudwatch-logs-vue.git
+                mv cloudwatch-logs-vue/* .
+                rm -r cloudwatch-logs-vue
+                npm install
+                npm run build
+                mv dist/* /var/www/html/
+              EOF
 
   tags = {
     Name = "iris_terraform_demo_ec2_instance"
--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,11 @@ output "API_DEMO_PATH_METHOD" {
   value       = "https://${aws_api_gateway_rest_api.cloudwatch_mock_api.id}.execute-api.eu-central-1.amazonaws.com/${aws_api_gateway_deployment.deployment.stage_name}/${aws_api_gateway_resource.demo_resource.path_part}"
 }
 
+output "API_LOGS_PATH_METHOD" {
+  description = "API Gateway URL to access the web page"
+  value       = "https://${aws_api_gateway_rest_api.cloudwatch_mock_api.id}.execute-api.eu-central-1.amazonaws.com/${aws_api_gateway_deployment.deployment.stage_name}/${aws_api_gateway_resource.logs_resource.path_part}"
+}
+
 /****************************************************
  * Output the RDS instance endpoint
  ****************************************************/
