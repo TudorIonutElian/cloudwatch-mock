77c7c1e3d095f47b16364feb6505720827d65f56
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-03-09 23:11:28 +0200

    Create cloud watch record

--- route53.tf
+++ route53.tf
@@ -17,3 +17,15 @@ resource "aws_route53_record" "api_domain_record" {
   records = ["${aws_api_gateway_rest_api.cloudwatch_mock_api.id}.execute-api.eu-central-1.amazonaws.com"]
   zone_id = data.aws_route53_zone.learndevtech.zone_id
 }
+
+/*****************************************************
+ * Create a record for the API Gateway
+ ****************************************************/
+resource "aws_route53_record" "cloudwatch_domain_record" {
+  name = "cloud-watch" # The subdomain (api.sumeet.life)
+  type = "CNAME"
+  ttl  = "300" # TTL in seconds
+
+  records = ["cloudwatch.learndevtech.com"]
+  zone_id = data.aws_route53_zone.learndevtech.zone_id
+}
