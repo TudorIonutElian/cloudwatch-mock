e32f44769d1caeb262d4335bc81e9de2971bdc6f
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-03-10 14:51:03 +0200

    Fix certificate

--- ssl-certificates.tf
+++ ssl-certificates.tf
@@ -2,7 +2,6 @@ resource "aws_acm_certificate" "learndevtech_com_cert" {
   domain_name               = "learndevtech.com"
   subject_alternative_names = ["*.learndevtech.com"]
   validation_method         = "DNS"
-  tags                      = { Name = "example-acm-cert-multiple-domains" }
 
   lifecycle {
     create_before_destroy = true
