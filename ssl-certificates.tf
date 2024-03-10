2b24f78ec491deae451d081e8e8b56ef072d17c0
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-03-10 14:49:03 +0200

    Update certificate

--- ssl-certificates.tf
+++ ssl-certificates.tf
@@ -1,5 +1,10 @@
-data "aws_acm_certificate" "learndevtech_com_cert" {
-  domain   = "*.learndevtech.com"
-  types       = ["AMAZON_ISSUED"]
-  most_recent = true
+resource "aws_acm_certificate" "learndevtech_com_cert" {
+  domain_name               = "learndevtech.com"
+  subject_alternative_names = ["*.learndevtech.com"]
+  validation_method         = "DNS"
+  tags                      = { Name = "example-acm-cert-multiple-domains" }
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
