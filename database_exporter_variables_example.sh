f9ad21ab37f24684d36724166dd2c4fef5dd0b05
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-03-15 22:31:45 +0200

    Test the sed replacer

---
+++ database_exporter_variables_example.sh
@@ -0,0 +1,13 @@
+############################################
+# Database Exporter Variables
+# This file contains the environment variables for the database exporter
+# -DATABASE_HOST: The host of the database
+# -DATABASE_USERNAME: The username of the database
+# -DATABASE_PASSWORD: The password of the database
+# -DATABASE_PORT: The port of the database
+############################################
+
+export DATABASE_HOST="DATABASE_HOST_TO_BE_REPLACED"
+export DATABASE_USERNAME="DATABASE_USERNAME_TO_BE_REPLACED"
+export DATABASE_PASSWORD="DATABASE_PASSWORD_TO_BE_REPLACED"
+export DATABASE_PORT="DATABASE_PORT_TO_BE_REPLACED"
