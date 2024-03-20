458a438ad9784fdea5a6cd12af07e82e38d1d925
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-03-20 23:20:18 +0200

    Document AMI filter

--- ami-filter.tf
+++ ami-filter.tf
@@ -2,6 +2,12 @@
 # Terraform module to filter AMI based on
 # architecture, startsWith, and endsWith
 ############################################
+ #*  Path: modules/ami-filter/main.tf
+ #*  This module is used to filter AMI based on architecture, startsWith, and endsWith
+ #*  The module takes the following input:
+ #*  - startsWith: The prefix of the AMI name
+ #*  - endsWith: The suffix of the AMI name
+ #*  - architecture: The architecture of the AMI
 
 module "ami_filter" {
   source = "./modules/ami-filter"
