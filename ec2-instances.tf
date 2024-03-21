7f3394688885f11116015b1d32b6d5641d500392
Tudor Ionut - Elian <ionuteliantudor@gmail.com>
2024-03-21 16:14:50 +0200

    Update ec2-instances.tf

--- ec2-instances.tf
+++ ec2-instances.tf
@@ -26,4 +26,12 @@ module "cloudwatch_ec2_staging" {
   ami_id        = module.ami_filter.ami_id
 }
 
+module "cloudwatch_ec2_production" {
+  source        = "./modules/ec2"
+  instance_type = "t2.micro"
+  key_name      = module.aws_key_pair.key_pair_name
+  domain_name   = "learndevtech.com"
+  ami_id        = module.ami_filter.ami_id
+}
+
 
